7ae688bd16bb6a2e6708c7d5b978b977ab6049ca
hitomi numari <h.h.hitomi1216@gmail.com>
2020-08-04 01:24:51 +0900

    解消：自分自身をフォロー問題→フォロー数がリアルタイムじゃない

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,9 @@ class User < ApplicationRecord
   has_many :passive_relationships, foreign_key: 'followed_id', class_name: 'Relationship', dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
-  
+
   def follow!(other_user)
-    active_relationships.create!(followed_id: other_user.id)
+    self.active_relationships.create!(followed_id: other_user.id) unless self == other_user
   end
   #フォローしているかどうかを確認する
   def following?(other_user)
